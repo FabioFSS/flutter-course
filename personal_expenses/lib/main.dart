b830ac9ef5bdad34307ca7e5443cc0eb0091aed7
Fabiano Santos <58434562+FabioFSS@users.noreply.github.com>
2022-07-26 23:33:47 -0300

    add form modal

--- personal_expenses/lib/main.dart
+++ personal_expenses/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/components/transaction_user.dart';
+import 'package:personal_expenses/components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main() {
   runApp(const ExpensesApp());
@@ -16,27 +19,84 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New running shoes',
+      value: 300.52,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Computer',
+      value: 2000.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionForm(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(
+          onSubmit: _addTransaction,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Personal Expenses'),
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionForm(context),
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
+          children: [
+            const Card(
               elevation: 10,
               child: Text('Graphics'),
             ),
-            TransactionUser(),
+            TransactionList(
+              transactions: _transactions,
+            ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openTransactionForm(context),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
